a4290d8f80c1f4d6ec093488b17287793219632f
mdevellano <mikedevellano@gmail.com>
2020-11-15 14:03:51 -0800

    First commit

---
+++ assets/custom.js
@@ -0,0 +1,49 @@
+/**
+ * Include your custom JavaScript here.
+ *
+ * We also offer some hooks so you can plug your own logic. For instance, if you want to be notified when the variant
+ * changes on product page, you can attach a listener to the document:
+ *
+ * document.addEventListener('variant:changed', function(event) {
+ *   var variant = event.detail.variant; // Gives you access to the whole variant details
+ * });
+ *
+ * You can also add a listener whenever a product is added to the cart:
+ *
+ * document.addEventListener('product:added', function(event) {
+ *   var variant = event.detail.variant; // Get the variant that was added
+ *   var quantity = event.detail.quantity; // Get the quantity that was added
+ * });
+ *
+ * If you just want to force refresh the mini-cart without adding a specific product, you can trigger the event
+ * "cart:refresh" in a similar way (in that case, passing the quantity is not necessary):
+ *
+ * document.documentElement.dispatchEvent(new CustomEvent('cart:refresh', {
+ *   bubbles: true
+ * }));
+ */
+
+document.addEventListener('variant:changed', function(event) {
+    var variant = event.detail.variant; // Gives you access to the whole variant details
+  console.log(variant.inventory_quantity);
+  if( variant.inventory_quantity < 1 ){
+  $('label.SizeSwatch.block-'+variant.id).addClass('ctmSold');
+  }
+  else{
+  $('label.SizeSwatch.block-'+variant.id).removeClass('ctmSold');
+  }
+  });
+
+
+
+  $(document).ready(function() {
+    $('body').on('click', '[name="checkout"], [name="goto_pp"], [name="goto_gc"]', function() {
+      if ($('#agree').is(':checked')) {
+        $(this).submit();
+      }
+      else {
+        alert("You must agree with the terms and conditions of sales to check out.");
+        return false;
+      }
+    });
+  });
